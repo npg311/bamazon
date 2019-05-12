b7c26dc1fc060312dbe2016bffee3f248ee0180b
npg311 <47582847+npg311@users.noreply.github.com>
2019-05-11 21:19:22 -0500

    attempt to call sql table data in vs

--- bamazonApp.sql
+++ bamazonApp.sql
@@ -8,3 +8,21 @@ CREATE TABLE `products` (
   `stock_quantity` int(11) NOT NULL,
   PRIMARY KEY (`item_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1
+
+# item_id, product_name, department_name, price, stock_quantity
+6, ryzen 7 2700x, electronics, 310, 15
+
+'rtx2080ti'
+
+'4', 'socks', 'clothing', '1', '1100'
+# item_id, product_name, department_name, price, stock_quantity
+'1', 'bananas ', 'grocery', '0', '100'
+3	headphones	electronics	150	4
+'mulch'
+'7', 'mulch', 'lawn/garden', '15', '65'
+'mulch'
+item_id, product_name, department_name, price, stock_quantity
+# item_id, product_name, department_name, price, stock_quantity
+3, headphones, electronics, 150, 4
+`bamazon`.`products`
+bamazon.products	analyze	status	OK
