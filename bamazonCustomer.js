6ca202ac141e8e5e0c4bf14c1870b8bd3738fc62
npg311 <47582847+npg311@users.noreply.github.com>
2019-05-11 19:47:55 -0500

    transfer of all files from laptop to home pc hopefully

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
   host: "localhost",
 
   
-  port: 3000,
+  port: 3306,
 
   // Your username
   user: "root",
@@ -16,3 +16,32 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+  });
+  
+  // function which prompts the user for what action they should take
+  function start() {
+    inquirer
+      .prompt({
+        name: "postOrBid",
+        type: "list",
+        message: "Would you like to [POST] an auction or [BID] on an auction?",
+        choices: ["POST", "BID", "EXIT"]
+      })
+      .then(function(answer) {
+        // based on their answer, either call the bid or the post functions
+        if (answer.postOrBid === "POST") {
+          postAuction();
+        }
+        else if(answer.postOrBid === "BID") {
+          bidAuction();
+        } else{
+          connection.end();
+        }
+      });
+  }
+  
