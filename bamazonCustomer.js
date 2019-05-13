3b8502a3991776c4d030368a641f663862024e4f
npg311 <47582847+npg311@users.noreply.github.com>
2019-05-13 18:49:38 -0500

    almost working..!

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,5 @@
-const mysql = require("mysql");
-const inquirer = require("inquirer");
+var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 
 var connection = mysql.createConnection({
@@ -25,29 +25,70 @@ var connection = mysql.createConnection({
   
   connection.connect(function(err) {
     if (err) throw err;
-    console.log("connected as customer " + connection.threadId);
+    console.log("--------------------------------------------- ");
+    console.log("--------------------------------------------- ");
+    console.log("-------------Welcome to BAMAZON!------------- ");
+    console.log("--------------------------------------------- ");
+    console.log("--------------------------------------------- ");
+    console.log("You are Customer Number " + connection.threadId);
+    console.log("Please select which product you would like to purchase!");
+    console.log("");
     queryAllProducts();
    /* queryDanceSongs(); */
   });
   
   function queryAllProducts() {
     connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
       for (var i = 0; i < res.length; i++) {
-        console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+        console.log("Item #: " + res[i].item_id + " | " +
+                    "Product: " + res[i].product_name + " | " +
+                    "Department " + res[i].department_name + " | " +
+                    "Price: " + res[i].price + " | " +
+                    "In Stock: " + res[i].stock_quantity);
+      
+      console.log("---------------------------------------------");
       }
-      console.log("-----------------------------------");
+      purchaseItem();
     });
   }
-  function start() {
+   var purchaseItem = function() {
     inquirer
-      .prompt({
-        name: "WhichProduct",
-        type: "list",
-        message: "Which product would you like to buy? (By product_id)",
-        choices: [SELECT * queryAllProducts],
-        
-      })
-  }
+      .prompt([{
+        name: "itemId",
+        type: "input",
+        message: "Which product would you like to buy (ID #)?"
+      }, 
+      {
+        name: "quantity",
+        type: "input",
+        message: "How many  /* + selectedItem + */ would you like to buy?"
+        //choices: [SELECT * queryAllProducts],
+        }]).then(function(answer){
+          connection.query("SELECT * FROM products", function(err, res) {
+            if (err) throw err;
+
+            var selectedItem;
+            for (var i = 0; i < res.length; i++) {
+              if (res[i].item_id === parseInt(answer.item_id)) {
+                selectedItem = res[i];
+              }
+            }
+            if (selectedItem.stock_quantity > parseInt(answer.quantity)) {
+              connection.query("UPDATE products SET ? WHERE ?",
+             [{ stock_quantity: (selectedItem.stock_quantity - parseInt(answer.quantity))},
+            { item_id: selectedItem.item_id}],
+            function(error) {
+              if (error) throw error;
+               console.log("Thank you for shopping with us today! Your bill has came to $" + parseInt(answer.quantity) * selectedItem.price);
+            });
+            } else {
+              console.log("I'm sorry, but we do not have enough  + selectedItem.item_id + in stock right now...");
+            }
+          });
+        });
+      };
+  
   
   /*
   function query() {
