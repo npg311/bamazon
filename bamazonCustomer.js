7c42151e255de48d15568a71691a40949a7a0299
npg311 <47582847+npg311@users.noreply.github.com>
2019-05-13 19:58:36 -0500

    final working customer portal for MYsql BAMAZON_db

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -57,20 +57,20 @@ var connection = mysql.createConnection({
       .prompt([{
         name: "itemId",
         type: "input",
-        message: "Which product would you like to buy (ID #)?"
+        message: "Which product would you like to buy (Item #)?"
       }, 
       {
         name: "quantity",
         type: "input",
-        message: "How many  /* + selectedItem + */ would you like to buy?"
-        //choices: [SELECT * queryAllProducts],
+        message: "How many  + selectedItem.product_name +  would you like to buy?",
+         //choices: [SELECT * queryAllProducts],
         }]).then(function(answer){
           connection.query("SELECT * FROM products", function(err, res) {
             if (err) throw err;
 
             var selectedItem;
             for (var i = 0; i < res.length; i++) {
-              if (res[i].item_id === parseInt(answer.item_id)) {
+              if (res[i].item_id === parseInt(answer.itemId)) {
                 selectedItem = res[i];
               }
             }
@@ -83,176 +83,11 @@ var connection = mysql.createConnection({
                console.log("Thank you for shopping with us today! Your bill has came to $" + parseInt(answer.quantity) * selectedItem.price);
             });
             } else {
-              console.log("I'm sorry, but we do not have enough  + selectedItem.item_id + in stock right now...");
+              console.log("I'm sorry, but we do not have enough "  + selectedItem.product_name + " in stock right now...");
             }
           });
         });
       };
   
   
-  /*
-  function query() {
-    var query = connection.query("SELECT * FROM songs WHERE genre=?", ["Dance"], function(err, res) {
-      for (var i = 0; i < res.length; i++) {
-        console.log(res[i].id + " | " + res[i].title + " | " + res[i].artist + " | " + res[i].genre);
-      }
-    });
   
-    // logs the actual query being run
-    console.log(query.sql);
-  }
-    // function which prompts the user for what action they should take
-  function start() {
-    inquirer
-      .prompt({
-        name: "postOrBid",
-        type: "list",
-        message: "Would you like to [POST] an auction or [BID] on an auction?",
-        choices: ["POST", "BID", "EXIT"]
-      })
-      .then(function(answer) {
-        // based on their answer, either call the bid or the post functions
-        if (answer.postOrBid === "POST") {
-          postAuction();
-        }
-        else if(answer.postOrBid === "BID") {
-          bidAuction();
-        } else{
-          connection.end();
-        }
-      });
-  }
-  
-  // function to handle posting new items up for auction
-  function postAuction() {
-    // prompt for info about the item being put up for auction
-    inquirer
-      .prompt([
-        {
-          name: "item",
-          type: "input",
-          message: "What is the item you would like to submit?"
-        },
-        {
-          name: "category",
-          type: "input",
-          message: "What category would you like to place your auction in?"
-        },
-        {
-          name: "startingBid",
-          type: "input",
-          message: "What would you like your starting bid to be?",
-          validate: function(value) {
-            if (isNaN(value) === false) {
-              return true;
-            }
-            return false;
-          }
-        }
-      ])
-      .then(function(answer) {
-        // when finished prompting, insert a new item into the db with that info
-        connection.query(
-          "INSERT INTO auctions SET ?",
-          {
-            item_name: answer.item,
-            category: answer.category,
-            starting_bid: answer.startingBid || 0,
-            highest_bid: answer.startingBid || 0
-          },
-          function(err) {
-            if (err) throw err;
-            console.log("Your auction was created successfully!");
-            // re-prompt the user for if they want to bid or post
-            start();
-          }
-        );
-      });
-  }
-  
-  function bidAuction() {
-    // query the database for all items being auctioned
-    connection.query("SELECT * FROM auctions", function(err, results) {
-      if (err) throw err;
-      // once you have the items, prompt the user for which they'd like to bid on
-      inquirer
-        .prompt([
-          {
-            name: "choice",
-            type: "rawlist",
-            choices: function() {
-              var choiceArray = [];
-              for (var i = 0; i < results.length; i++) {
-                choiceArray.push(results[i].item_name);
-              }
-              return choiceArray;
-            },
-            message: "What auction would you like to place a bid in?"
-          },
-          {
-            name: "bid",
-            type: "input",
-            message: "How much would you like to bid?"
-          }
-        ])
-        .then(function(answer) {
-          // get the information of the chosen item
-          var chosenItem;
-          for (var i = 0; i < results.length; i++) {
-            if (results[i].item_name === answer.choice) {
-              chosenItem = results[i];
-            }
-          }
-  
-          // determine if bid was high enough
-          if (chosenItem.highest_bid < parseInt(answer.bid)) {
-            // bid was high enough, so update db, let the user know, and start over
-            connection.query(
-              "UPDATE auctions SET ? WHERE ?",
-              [
-                {
-                  highest_bid: answer.bid
-                },
-                {
-                  id: chosenItem.id
-                }
-              ],
-              function(error) {
-                if (error) throw err;
-                console.log("Bid placed successfully!");
-                start();
-              }
-            );
-          }
-          else {
-            // bid wasn't high enough, so apologize and start over
-            console.log("Your bid was too low. Try again...");
-            start();
-          }
-        });
-    });
-  }
-  
-
-  // function which prompts the user for what action they should take
-  function start() {
-    inquirer
-      .prompt({
-        name: "postOrBid",
-        type: "list",
-        message: "Would you like to [POST] an auction or [BID] on an auction?",
-        choices: ["POST", "BID", "EXIT"]
-      })
-      .then(function(answer) {
-        // based on their answer, either call the bid or the post functions
-        if (answer.postOrBid === "POST") {
-          postAuction();
-        }
-        else if(answer.postOrBid === "BID") {
-          bidAuction();
-        } else{
-          connection.end();
-        }
-      });
-  }
-  */
